1f5a7ddf6c700cd1c6cb9b6d7d3db569fb68d35d
Jarek Wojciechowski <jarekwojo@gmail.com>
2021-08-31 11:18:17 -0400

    work on the starfield wrapping

--- sketch.js
+++ sketch.js
@@ -2,9 +2,11 @@ const WIDTH = 800;
 const HEIGHT = 800;
 const TURN_SPEED = 4;
 const SLOWING_FRICTION = 0.99;
-const THRUSTER_POWER = 4;
+const THRUSTER_POWER = 2;
 const MAX_VEL = 200;
-const NUM_STARS = 10;
+const NUM_STARS = 20;
+const STARFIELD_WIDTH = WIDTH * 2;
+const STARFIELD_HEIGHT = HEIGHT * 2;
 
 let gState = {
   direction: 0, // Measured from 3 o'clock
@@ -23,11 +25,12 @@ function setup() {
 }
 
 const generateStarfield = (state) => {
+  // Stars feel kinda clumped in blocks when moving
   for (let depth = 1; depth <= 6; depth++) {
     for (let i = 0; i < NUM_STARS * depth; i++) {
       state.stars.push({
-        x: random(0, WIDTH * 2),
-        y: random(0, HEIGHT * 2),
+        x: random(0, STARFIELD_WIDTH),
+        y: random(0, STARFIELD_HEIGHT),
         depth,
       });
     }
@@ -48,6 +51,8 @@ const drawShip = (state) => {
   translate(WIDTH / 2, HEIGHT / 2);
   rotate(state.direction);
   strokeWeight(1);
+  // Add some more visual interest to the thruster
+  // Flickering, crackling and popping when it turns off
   if (state.thruster === 'on') {
     fill('blue');
     stroke('magenta');
@@ -66,14 +71,20 @@ Number.prototype.mod = function (n) {
 const drawStarfield = (state) => {
   state.stars.map(({ x, y, depth }) => {
     push();
-    const starX = (state.x / depth + x).mod(WIDTH);
-    const starY = (state.y / depth + y).mod(HEIGHT);
+    const starX =
+      (state.x / depth + x).mod(STARFIELD_WIDTH) - // Constrain to tile size
+      (STARFIELD_WIDTH - WIDTH) / 2; // Offset to align center of the tile with the center of the viewable area
+    const starY =
+      (state.y / depth + y).mod(STARFIELD_HEIGHT) -
+      (STARFIELD_HEIGHT - HEIGHT) / 2;
     drawStar(starX, starY, 5 - depth / 2);
     pop();
   });
 };
 
 const drawStar = (x, y, size) => {
+  // Maybe make the stars dim/fade or draw a cross shape instead
+  // Play around with different star colors
   circle(x, y, size);
 };
 
@@ -95,6 +106,8 @@ const handleInput = (state) => {
 };
 
 const processState = (state) => {
+  // Thruster more powerful over time
+  // I want the ship to have more heft as it turns, instead of just discarding momentum
   if (state.thruster === 'on') {
     state.xVel += cos(state.direction) * THRUSTER_POWER;
     state.yVel += sin(state.direction) * THRUSTER_POWER;
@@ -106,10 +119,11 @@ const processState = (state) => {
 
   state.xVel *= SLOWING_FRICTION;
   state.yVel *= SLOWING_FRICTION;
+  console.log(state.xVel, state.yVel);
   state.xVel = constrain(state.xVel, -1 * MAX_VEL, MAX_VEL);
   state.yVel = constrain(state.yVel, -1 * MAX_VEL, MAX_VEL);
 };
 
 // Keep a momentum vector
 // The > the delta beweeen momentum and pointed direction
-// the more
+// the more momentum gets sent laterally?
